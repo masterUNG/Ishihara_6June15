a195b79da0487294256ddfb57a5dfb1bcf97ca60
masterUNG <phrombutr@gmail.com>
2015-06-06 15:04:04 +0700

    Day1

--- app/src/main/java/appewtc/masterung/ishihara/MainActivity.java
+++ app/src/main/java/appewtc/masterung/ishihara/MainActivity.java
@@ -1,17 +1,86 @@
 package appewtc.masterung.ishihara;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView questionTextView;
+    private ImageView ishiharaImageView;
+    private RadioGroup choiceRadioGroup;
+    private RadioButton choice1RadioButton, choice2RadioButton,
+            choice3RadioButton, choice4RadioButton;
+    private Button answerButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+        //Radio Controller
+        radioController();
+
+    }   // onCreate
+
+    private void radioController() {
+
+        choiceRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                //Sound Effect
+                MediaPlayer radioPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_shut);
+                radioPlayer.start();
+
+            }   // event
+        });
+
+    }   // radioController
+
+    private void buttonController() {
+
+        answerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer buttonPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_long);
+                buttonPlayer.start();
+
+            }   // event
+        });
+
+    }   // buttonController
+
+    private void bindWidget() {
+
+        questionTextView = (TextView) findViewById(R.id.textView2);
+        ishiharaImageView = (ImageView) findViewById(R.id.imageView);
+        choiceRadioGroup = (RadioGroup) findViewById(R.id.ragChoice);
+        choice1RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        choice2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        choice3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        choice4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+        answerButton = (Button) findViewById(R.id.button);
+
+    }   // bindWidget
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,4 +103,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+}   // Main Class
